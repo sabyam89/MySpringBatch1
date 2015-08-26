b5ad2cf160f3f4cd4763bf74cd2d36c21b4f158a
sabyam007 <sabyam007@gmail.com>
2015-08-27 01:51:35 +0530

    commit

--- src/main/java/com/saby/MySpringBatch1/CustomFileReader.java
+++ src/main/java/com/saby/MySpringBatch1/CustomFileReader.java
@@ -2,6 +2,7 @@ package com.saby.MySpringBatch1;
 
 import java.util.ArrayList;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemStreamException;
 import org.springframework.batch.item.NonTransientResourceException;
@@ -13,61 +14,71 @@ import org.springframework.core.io.Resource;
 import com.saby.MySpringBatch1.Entity.Hospital;
 
 public class CustomFileReader implements
-        ResourceAwareItemReaderItemStream<Object> {
+ResourceAwareItemReaderItemStream<Object> {
 
-    private Object curItem = null;
-    
-    
+	private String curItem = null;
 
-    private ResourceAwareItemReaderItemStream<Object> delegate;
-    
-    public Object read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
-		 if (curItem == null) {
-	            curItem = (String) delegate.read();
-	        }
-		 	int count=0;
-		 	StringBuilder item = (StringBuilder) curItem;
-		 	count = curItem.toString().split("$").length-1;
-	        curItem = null;
 
-	        while(count<=3) {
-	        	peek();
-	        	count+= curItem.toString().split("$").length-1;
-	            if (count<=3) {
-	            	item.append(curItem);
-	            }
-	            curItem = null;
-	        }
 
-	        return item;
+	private ResourceAwareItemReaderItemStream<Object> delegate;
+
+	public Object read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+		if (curItem == null) {
+			String[] fieldSet = (String[]) delegate.read();
+			curItem = fieldSet[0];
+
+		}
+		int count=0;
+		StringBuilder item = new StringBuilder(curItem);
+		count = StringUtils.countMatches(curItem, "$");
+		curItem = null;
+
+		while(count<=3) {
+			peek();
+			count+= StringUtils.countMatches(curItem, "$");
+			if (count<=3) {
+				item.append(curItem);
+				curItem = null;
+			}
+		}
+		Hospital record= new Hospital();
+		String[] values = StringUtils.split(item.toString(),"$");
+		record.setHospitalName(values[0]);
+		record.setAddress(values[1]);
+		record.setCity(values[2]);
+		record.setNetwork(values[3]);
+		return record;
 	}
 
-    public Object peek() throws Exception, UnexpectedInputException,
-            ParseException {
-        if (curItem == null) {
-            curItem = delegate.read();
-        }
-        return curItem;
-    }
-
-    public void setDelegate(ResourceAwareItemReaderItemStream<Object> delegate) {
-        this.delegate = delegate;
-    }
-
-    public void close() throws ItemStreamException {
-        delegate.close();
-    }
-
-    public void open(ExecutionContext arg0) throws ItemStreamException {
-        delegate.open(arg0);
-    }
-
-    public void update(ExecutionContext arg0) throws ItemStreamException {
-        delegate.update(arg0);
-    }
-
-    public void setResource(Resource arg0) {
-        delegate.setResource(arg0);
-    }
-
+	public Object peek() throws Exception, UnexpectedInputException,
+	ParseException {
+		if (curItem == null) {
+			String[] fieldSet = (String[]) delegate.read();
+			if(null!=fieldSet){
+				curItem = fieldSet[0];
+			}
+		}
+		return curItem;
 	}
+
+	public void setDelegate(ResourceAwareItemReaderItemStream<Object> delegate) {
+		this.delegate = delegate;
+	}
+
+	public void close() throws ItemStreamException {
+		delegate.close();
+	}
+
+	public void open(ExecutionContext arg0) throws ItemStreamException {
+		delegate.open(arg0);
+	}
+
+	public void update(ExecutionContext arg0) throws ItemStreamException {
+		delegate.update(arg0);
+	}
+
+	public void setResource(Resource arg0) {
+		delegate.setResource(arg0);
+	}
+
+}
